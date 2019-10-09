04688a9dc1601aca0ac042af3f9d702d7f8f0de1
NecrOctopuS <53893624+NecrOctopuS@users.noreply.github.com>
2019-10-10 00:34:52 +0300

    Заменил циклы на comprehensions

--- definition_participants_list_for_Instagram.py
+++ definition_participants_list_for_Instagram.py
@@ -34,14 +34,8 @@ if __name__ == "__main__":
     comments = bot.get_media_comments_all(post_id)
     users_with_real_friend = []
     for comment in comments:
-        usernames = get_users_from_comment(comment['text'])
-        for username in usernames:
-            if is_user_exist(username):
-                friend_not_fake = True
-                break
-            else:
-                friend_not_fake = False
-        if friend_not_fake:
+        print(comment)
+        if any(is_user_exist(username) for username in get_users_from_comment(comment['text'])):
             users_with_real_friend.append((comment['user']['pk'], comment['user']['username']))
     users_id_who_like_post = bot.get_media_likers(post_id)
     user_followers = bot.get_user_followers(bot.get_user_id_from_username(post_username))
@@ -49,7 +43,6 @@ if __name__ == "__main__":
     for user_id, username in users_with_real_friend:
         if str(user_id) in users_id_who_like_post and str(user_id) in user_followers:
             users_who_meet_conditions.append((user_id, username))
-
     print('Список участников: ', )
     for user_id, username in set(users_who_meet_conditions):
         print(username, end=' ')
